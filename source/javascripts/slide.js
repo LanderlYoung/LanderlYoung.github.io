47bd3ee5421762d01eeb0b8bdc5e141acfe65f79
LanderlYoung <LanderlYoung@gmail.com>
2014-01-14 17:16:27 +0800

    good

--- source/javascripts/slide.js
+++ source/javascripts/slide.js
@@ -2,5 +2,37 @@
 	var old_onload = window.onload || function(){};
 	window.onload = function() {
 		old_onload();
+		var hint = document.getElementById("hint");
+		var slide = document.getElementById("slide");
+		var container = document.getElementsByClassName("container")[0];
+
+		hint.addEventListener("click", function() {
+			hint.style["display"] = "none";
+			move(slide, -23, 0, 0.5, 20);
+		});
+
+		container.addEventListener("click", function(){
+			console.log("container");
+		});
+
+		var ctn_old_click = container["onclick"] || function(){};
+		container.addEventListener("click", function() {
+			ctn_old_click();
+			if(hint.style["display"] !== "block") {
+				hint.style["display"] = "block";
+			}
+		});
+
+		function move(obj, from, to, duration, steps) {
+			var step_len = (to - from) / steps;
+			var pos = from;
+			var handle = window.setInterval(function() {
+				if(Math.abs(pos - to) < 0.1) {
+					window.clearInterval(handle);
+				}
+				obj.style["left"] = pos + "%";
+				pos += step_len;
+			}, duration / steps);
+		}
 	}
 })();
